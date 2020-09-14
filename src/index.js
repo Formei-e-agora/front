1e03ae96b1e32f08cabbb1dab455f2ceef4416d8
Leonardo Said <leonardocosta013@gmail.com>
2020-09-14 20:17:32 -0300

    feat: folder structure

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import store from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import App from './views/App/App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/views/App/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <p>app</p>
+  );
+}
+
+export default App;
