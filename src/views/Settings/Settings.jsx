a7beab20c1439e4e212805a820b9e3757e99bd24
Leonardo Said <leonardocosta013@gmail.com>
2020-10-02 14:29:24 -0300

    feat(): basic views

--- src/views/Settings/Settings.jsx
+++ src/views/Settings/Settings.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
-import { Layout } from '../../components/index';
+import { Row, Col, Card, Tabs } from 'antd';
+import { Layout, UserCard } from '../../components/index';
+
+const { TabPane } = Tabs;
 
 const Settings = (props) => {
     return (
         <Layout currentLocation={props.history.location.pathname}>
-            <p>Settings</p>
+            <Row justify="center" gutter={16}>
+                <Col xxl={4} xl={6}>
+                    <UserCard />
+                </Col>
+                <Col xxl={12} xl={14}>
+                    <Card>
+                        <Tabs defaultActiveKey="1">
+                            <TabPane tab="Perfil" key="1">
+                                Form para alterar dados de perfil/contato
+                            </TabPane>
+                            <TabPane tab="Endereço" key="2">
+                                Form para alterar dados do seu endereço
+                            </TabPane>
+                            <TabPane tab="Senha" key="3">
+                                Form para alterar senha/dados de login
+                            </TabPane>
+                        </Tabs>
+                    </Card>
+                </Col>
+            </Row>
         </Layout>
     );
 }
